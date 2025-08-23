18ae04d2a7e603c992d97b5243e2f170dcf997b5
Alfin <alfinthumbanath@gmail.com>
2025-08-23 13:42:46 +0530

    update gradle update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24"
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.google.dagger.hilt.android)
 }
 
 android {
